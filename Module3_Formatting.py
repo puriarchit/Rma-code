6b85e10f0e3808510bede73371babd36a2f67ff7
ARCHIT PURI <156986776+puriarchit@users.noreply.github.com>
2026-08-03 13:58:00 +0530

    Update Module3_Formatting.py

--- Module3_Formatting.py
+++ Module3_Formatting.py
@@ -547,7 +547,7 @@ start_time = time.time()
 
 # Optimization: Drop index if exists and recreate it to speed up partitioned count!
 cursor.execute("DROP INDEX IF EXISTS IX_EntityRemark_EntityGUID ON EntityRemark")
-cursor.execute("CREATE NONCLUSTERED INDEX IX_EntityRemark_EntityGUID ON EntityRemark(EntityGUID) INCLUDE (Remark, EntityRemarkGUID, LastUpdated)")
+cursor.execute("CREATE NONCLUSTERED INDEX IX_EntityRemark_EntityGUID ON EntityRemark(EntityGUID)")
 conn.commit()
 
 cursor.execute("IF OBJECT_ID('EntityRemark_DUP', 'U') IS NOT NULL DROP TABLE EntityRemark_DUP")
@@ -646,5 +646,6 @@ global_end = time.time()
 total_time = (global_end - global_start) / 60
 
 print(f"Process completed. Total time: {total_time:.2f} minutes.")
+
 
 
