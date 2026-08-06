062c476b7db808d37727952a1fba049d2df046e7
ARCHIT PURI <156986776+puriarchit@users.noreply.github.com>
2026-08-06 07:24:06 +0530

    Update Module4_Consolidation.py

--- Module4_Consolidation.py
+++ Module4_Consolidation.py
@@ -47,6 +47,21 @@ global_start = time.time()
 print("indexing staging tables for fast merge joins...")
 index_start = time.time()
 
+# Index raw staging tables used in the join to fully optimize join plan
+raw_index_queries = [
+    ("IX_Entity_EntityGUID", "Entity", "EntityGUID"),
+    ("IX_EntityEnforcement_EntityGUID", "EntityEnforcement", "EntityGUID"),
+    ("IX_EntitySanction_EntityGUID", "EntitySanction", "EntityGUID")
+]
+for idx_name, tbl_name, col_name in raw_index_queries:
+    try:
+        cursor.execute(f"IF EXISTS (SELECT * FROM sys.indexes WHERE name = '{idx_name}') DROP INDEX {idx_name} ON {tbl_name}")
+        print(f"  creating non-clustered index on raw table {tbl_name}...")
+        cursor.execute(f"CREATE NONCLUSTERED INDEX {idx_name} ON {tbl_name}({col_name})")
+        conn.commit()
+    except Exception as ex:
+        print(f"  raw table index alert on {tbl_name}: {ex}")
+
 index_queries = [
     ("IX_EntityDOB_New_EntityGUID", "EntityDOB_New", "EntityGUID"),
     ("IX_EntityAddress_New_EntityGUID", "EntityAddress_New", "EntityGUID"),
@@ -76,46 +91,6 @@ for idx_name, tbl_name, col_name in index_queries:
 print(f"indexing staging tables completed, took {time.time() - index_start:.2f} seconds.")
 
 print("recreating staging tables...")
-cursor.execute("IF OBJECT_ID('NegativeList_New', 'U') IS NOT NULL DROP TABLE NegativeList_New")
-cursor.execute("""
-    CREATE TABLE [dbo].[NegativeList_New](
-        [EntityGUID] [nvarchar](50) NULL,
-        [ReferenceID] [nvarchar](50) NULL,
-        [EntityType] [nvarchar](50) NULL,
-        [Gender] [nvarchar](50) NULL,
-        [FirstName] [nvarchar](4000) NULL,
-        [LastName] [nvarchar](250) NULL,
-        [SecondName] [nvarchar](500) NULL,
-        [Title] [nvarchar](250) NULL,
-        [DOB] [nvarchar](92) NULL,
-        [ALTDOB1] [datetime] NULL,
-        [ALTDOB2] [datetime] NULL,
-        [ALTDOB3] [datetime] NULL,
-        [AddressLine1] [nvarchar](255) NULL,
-        [AddressLine2] [nvarchar](255) NULL,
-        [City] [nvarchar](50) NULL,
-        [Country] [nvarchar](100) NULL,
-        [POB] [nvarchar](50) NULL,
-        [WLType] [nvarchar](50) NULL,
-        [OriginalSource] [nvarchar](MAX) NULL,
-        [Remark] [nvarchar](4000) NULL,
-        [NationalIDInfo] [nvarchar](250) NULL,
-        [NationalIDNo] [nvarchar](250) NULL,
-        [IdOtherInfo1] [nvarchar](250) NULL,
-        [IdNo1] [nvarchar](250) NULL,
-        [IdOtherInfo2] [nvarchar](250) NULL,
-        [IdNo2] [nvarchar](250) NULL,
-        [IdOtherInfo3] [nvarchar](250) NULL,
-        [IdNo3] [nvarchar](250) NULL,
-        [IdOtherInfo4] [nvarchar](250) NULL,
-        [IdNo4] [nvarchar](250) NULL,
-        [IdOtherInfo5] [nvarchar](250) NULL,
-        [IdNo5] [nvarchar](250) NULL,
-        [Nationality] [nvarchar](4000) NULL,
-        [Citizenship] [nvarchar](100) NULL
-    )
-""")
-
 cursor.execute("IF OBJECT_ID('NegativeList_New1', 'U') IS NOT NULL DROP TABLE NegativeList_New1")
 cursor.execute("""
     CREATE TABLE [dbo].[NegativeList_New1](
@@ -179,6 +154,7 @@ cursor.execute("""
 """)
 cursor.execute("CREATE CLUSTERED INDEX IX_PEP_GUIDs_EntityGUID ON #PEP_GUIDs(EntityGUID)")
 conn.commit()
+
 try:
     cursor.execute("TRUNCATE TABLE EntityCountryAssociation")
     cursor.execute("CHECKPOINT")
@@ -188,8 +164,10 @@ print(f"lookup tables created, took {time.time() - step_start:.2f} seconds.")
 
 print("assembling master profile details...")
 step_start = time.time()
+
+# 1. Load non-PEP profiles and consolidated watchlist matches directly into NegativeList_New1
 cursor.execute("""
-    INSERT INTO NegativeList_New WITH (TABLOCK) (
+    INSERT INTO NegativeList_New1 WITH (TABLOCK) (
         EntityGUID, ReferenceID, EntityType, Gender, FirstName, LastName, SecondName, Title,
         DOB, ALTDOB1, ALTDOB2, ALTDOB3, AddressLine1, AddressLine2, City, Country, POB,
         WLType, OriginalSource, Remark, NationalIDInfo, NationalIDNo,
@@ -232,6 +210,7 @@ cursor.execute("""
         J.Nationality,
         K.Citizenship
     FROM Entity A WITH (NOLOCK)
+    LEFT JOIN #PEP_GUIDs p ON A.EntityGUID = p.EntityGUID
     LEFT JOIN EntityDOB_New B WITH (NOLOCK) ON A.EntityGUID = B.EntityGUID
     LEFT JOIN EntityAddress_New C WITH (NOLOCK) ON A.EntityGUID = C.EntityGUID
     LEFT JOIN EntityRemark_New D WITH (NOLOCK) ON A.EntityGUID = D.EntityGUID
@@ -242,34 +221,16 @@ cursor.execute("""
     LEFT JOIN EntityIdentification_New I WITH (NOLOCK) ON A.EntityGUID = I.EntityGUID
     LEFT JOIN #TempNationalities J ON A.EntityGUID = J.EntityGUID
     LEFT JOIN Entity_Citizenship_New K WITH (NOLOCK) ON A.EntityGUID = K.EntityGUID
+    WHERE p.EntityGUID IS NULL OR (isnull(F.SourceName, G.SourceName) IS NOT NULL AND isnull(F.SourceName, G.SourceName) <> 'PEP')
     OPTION (MERGE JOIN, HASH JOIN, MIN_GRANT_PERCENT = 20)
 """)
 conn.commit()
-rows_master = cursor.execute("SELECT COUNT(*) FROM NegativeList_New WITH (NOLOCK)").fetchone()[0]
-print(f"master profiles consolidated ({rows_master} rows), took {time.time() - step_start:.2f} seconds.")
+print(f"non-PEP and consolidated profiles loaded, took {time.time() - step_start:.2f} seconds.")
 
-print("splitting profiles into watchlist categories...")
+print("loading PEP category profiles...")
 step_start = time.time()
 
-cursor.execute("""
-    INSERT INTO NegativeList_New1 WITH (TABLOCK)
-    SELECT n.* 
-    FROM NegativeList_New n WITH (NOLOCK)
-    LEFT JOIN #PEP_GUIDs p ON n.EntityGUID = p.EntityGUID
-    WHERE n.WLType IS NULL AND p.EntityGUID IS NULL
-    OPTION (MERGE JOIN, HASH JOIN, MIN_GRANT_PERCENT = 10)
-""")
-conn.commit()
-
-cursor.execute("""
-    INSERT INTO NegativeList_New1 WITH (TABLOCK)
-    SELECT * 
-    FROM NegativeList_New WITH (NOLOCK)
-    WHERE WLType IS NOT NULL AND WLType <> 'PEP'
-    OPTION (MERGE JOIN, HASH JOIN, MIN_GRANT_PERCENT = 10)
-""")
-conn.commit()
-
+# 2. Load PEP profiles directly into NegativeList_New1
 cursor.execute("""
     INSERT INTO NegativeList_New1 WITH (TABLOCK) (
         EntityGUID, ReferenceID, EntityType, Gender, FirstName, LastName, SecondName, Title,
@@ -279,18 +240,56 @@ cursor.execute("""
         Nationality, Citizenship
     )
     SELECT 
-        n.EntityGUID, n.ReferenceID, n.EntityType, n.Gender, n.FirstName, n.LastName, n.SecondName, n.Title,
-        n.DOB, n.ALTDOB1, n.ALTDOB2, n.ALTDOB3, n.AddressLine1, n.AddressLine2, n.City, n.Country, n.POB,
-        'PEP' AS WLType, n.OriginalSource, n.Remark, n.NationalIDInfo, n.NationalIDNo,
-        n.IdOtherInfo1, n.IdNo1, n.IdOtherInfo2, n.IdNo2, n.IdOtherInfo3, n.IdNo3, n.IdOtherInfo4, n.IdNo4, n.IdOtherInfo5, n.IdNo5,
-        n.Nationality, n.Citizenship
-    FROM NegativeList_New n WITH (NOLOCK)
-    INNER JOIN #PEP_GUIDs p ON n.EntityGUID = p.EntityGUID
+        A.EntityGUID,
+        CAST(SUBSTRING(A.EntityID, 1, 50) AS NVARCHAR(50)) as ReferenceID,
+        CAST(SUBSTRING(A.EntityTypeDesc, 1, 50) AS NVARCHAR(50)) as EntityType,
+        CAST(SUBSTRING(A.Gender, 1, 50) AS NVARCHAR(50)) as Gender,
+        CAST(SUBSTRING(ISNULL(A.FirstName,'') + ' ' + ISNULL(A.MiddleName,''), 1, 4000) AS NVARCHAR(4000)) as FirstName,
+        CAST(SUBSTRING(A.LastName, 1, 250) AS NVARCHAR(250)) as LastName,
+        CAST(SUBSTRING(A.Name, 1, 500) AS NVARCHAR(500)) as SecondName,
+        CAST(SUBSTRING(A.Title, 1, 250) AS NVARCHAR(250)) as Title,
+        B.DOB,
+        B.ALTDOB1,
+        B.ALTDOB2,
+        B.ALTDOB3,
+        C.AddressLine1,
+        C.AddressLine2,
+        C.City,
+        C.Country,
+        C.POB,
+        'PEP' AS WLType,
+        E.SourceURI as OriginalSource,
+        D.Remark,
+        H.IdentificationTypeDesc as NationalIDInfo,
+        H.IdentificationNumber as NationalIDNo,
+        I.IdOtherInfo1,
+        I.IdNo1,
+        I.IdOtherInfo2,
+        I.IdNo2,
+        I.IdOtherInfo3,
+        I.IdNo3,
+        I.IdOtherInfo4,
+        I.IdNo4,
+        I.IdOtherInfo5,
+        I.IdNo5,
+        J.Nationality,
+        K.Citizenship
+    FROM Entity A WITH (NOLOCK)
+    INNER JOIN #PEP_GUIDs p ON A.EntityGUID = p.EntityGUID
+    LEFT JOIN EntityDOB_New B WITH (NOLOCK) ON A.EntityGUID = B.EntityGUID
+    LEFT JOIN EntityAddress_New C WITH (NOLOCK) ON A.EntityGUID = C.EntityGUID
+    LEFT JOIN EntityRemark_New D WITH (NOLOCK) ON A.EntityGUID = D.EntityGUID
+    LEFT JOIN EntitySourceItem_New E WITH (NOLOCK) ON A.EntityGUID = E.EntityGUID
+    LEFT JOIN EntityEnforcement F WITH (NOLOCK) ON A.EntityGUID = F.EntityGUID
+    LEFT JOIN EntitySanction G WITH (NOLOCK) ON A.EntityGUID = G.EntityGUID
+    LEFT JOIN EntityIdentification_National_New H WITH (NOLOCK) ON A.EntityGUID = H.EntityGUID
+    LEFT JOIN EntityIdentification_New I WITH (NOLOCK) ON A.EntityGUID = I.EntityGUID
+    LEFT JOIN #TempNationalities J ON A.EntityGUID = J.EntityGUID
+    LEFT JOIN Entity_Citizenship_New K WITH (NOLOCK) ON A.EntityGUID = K.EntityGUID
     OPTION (MERGE JOIN, HASH JOIN, MIN_GRANT_PERCENT = 10)
 """)
 conn.commit()
 
-cursor.execute("DROP TABLE IF EXISTS NegativeList_New")
 cursor.execute("DROP TABLE IF EXISTS #TempNationalities")
 cursor.execute("DROP TABLE IF EXISTS #PEP_GUIDs")
 conn.commit()
@@ -304,4 +303,3 @@ conn.close()
 global_end = time.time()
 total_time = (global_end - global_start) / 60
 print(f"module 4 completed in {total_time:.2f} minutes.")
-
